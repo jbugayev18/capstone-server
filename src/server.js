a0818a04bc78a6e301b5da9925dbbc7b81b3c728
Julia Bugayev <jbugayev18@gmail.com>
2020-09-11 11:07:15 -0700

    Updated

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = require("./app");
 const knex = require("knex");
 
-const { PORT, DB_URL } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
 
 
-const db = knex({ client: 'pg', connection: DB_URL, });
+const db = knex({ client: 'pg', connection: DATABASE_URL, });
 
 app.set('db', db);
 
