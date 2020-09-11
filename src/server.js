b66d65414f9f320db8e7c5de07439985ee1a6ded
Julia Bugayev <jbugayev18@gmail.com>
2020-09-11 12:51:23 -0700

    Updates

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const db = knex({ client: 'pg', connection: DATABASE_URL, });
 app.set('db', db);
 
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
 
